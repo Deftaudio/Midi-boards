a145464c112060052131eaef86972bb4128f21f9
Andrei Kudryavtsev <andrei.kudryavtsev@gmail.com>
2026-04-03 13:36:09 -0700

    Teensy 1U 8x14 Firmware 2.2
    Teensy 8x14 MIDI interface got real-time FSK generator and Sync48 (48ppqn) clock outs

---
+++ Teensy_1U_MIDI_8x14/Firmware/2.2.0/SysEx/test.sh
@@ -0,0 +1,7 @@
+#sendmidi dev "Teensy 1U Port 16" hex syx 7d [TRSPort# (9=0, 10=1, etc)] [MSB value] [LSB value] 7f
+#sendmidi dev "Teensy 1U Port 16" hex syx 7d 00 1F 00 7f
+#sendmidi dev "Teensy 1U Port 16" hex syx 7d 01 1F 00 7f
+#sendmidi dev "Teensy 1U Port 16" hex syx 7d 02 1F 00 7f
+sendmidi dev "Teensy 1U Port 16" hex syx 7d 03 00 00 7f
+#sendmidi dev "Teensy 1U Port 16" hex syx 7d 04 1F 00 7f
+#sendmidi dev "Teensy 1U Port 16" hex syx 7d 05 1F 00 7f
